7886d85a5c4105c68fe9c394691b298dc1cf1460
Adam Hooper <adam@adamhooper.com>
2011-11-28 12:21:24 -0500

    Downcase and remove "de " in CSV keys

--- lib/canonicalizer.rb
+++ lib/canonicalizer.rb
@@ -2,6 +2,8 @@ module Canonicalizer
   def self.canonicalize(s)
     normalized = s.to_s.mb_chars.normalize(:kd)
     ascii = normalized.gsub(/[^\x00-\x7F]/n, '').to_s
-    ascii.gsub(/[^-'a-zA-Z ]/, '').strip
+    ascii_simple = ascii.gsub(/[^-'a-zA-Z ]/, '').strip
+    ascii_no_articles = ascii_simple.gsub(/^(de |la |du |l'|d')*/i, '')
+    ascii_no_articles.downcase
   end
 end
--- spec/lib/canonicalizer_spec.rb
+++ spec/lib/canonicalizer_spec.rb
@@ -2,24 +2,32 @@ require 'spec_helper'
 
 describe(Canonicalizer) do
   describe('canonicalize') do
-    it('should leave most strings untouched') do
-      Canonicalizer.canonicalize('Tremblay').should == 'Tremblay'
+    it('should downcase') do
+      Canonicalizer.canonicalize('Tremblay').should == 'tremblay'
     end
 
     it('should remove numbers and underscores') do
-      Canonicalizer.canonicalize('Adfs_df32a').should == 'Adfsdfa'
+      Canonicalizer.canonicalize('Adfs_df32a').should == 'adfsdfa'
     end
 
     it('should leave dashes, spaces and apostrophes') do
-      Canonicalizer.canonicalize("O'Brien-Far Lee").should == "O'Brien-Far Lee"
+      Canonicalizer.canonicalize("O'Brien-Far Lee").should == "o'brien-far lee"
     end
 
     it('should convert accents into regular characters') do
-      Canonicalizer.canonicalize('Hǫöpér').should == 'Hooper'
+      Canonicalizer.canonicalize('Hǫöpér').should == 'hooper'
     end
 
     it('should remove leading and trailing spaces') do
-      Canonicalizer.canonicalize(' Hooper ').should == 'Hooper'
+      Canonicalizer.canonicalize(' Hooper ').should == 'hooper'
+    end
+
+    it('should remove "de" and "la" and "du"') do
+      Canonicalizer.canonicalize('de Hooper').should == 'hooper'
+      Canonicalizer.canonicalize('Du Hooper').should == 'hooper'
+      Canonicalizer.canonicalize('de la Hooper').should == 'hooper'
+      Canonicalizer.canonicalize('la Hooper').should == 'hooper'
+      Canonicalizer.canonicalize("d'Ooper").should == 'ooper'
     end
   end
 end
