a07a5c84ae28f661e4c95ee9848bf3526227b89a
Adam Hooper <adam@adamhooper.com>
2011-11-28 19:24:51 -0500

    "des" isn't a name

--- lib/canonicalizer.rb
+++ lib/canonicalizer.rb
@@ -3,7 +3,7 @@ module Canonicalizer
     normalized = s.to_s.mb_chars.normalize(:kd)
     ascii = normalized.gsub(/[^\x00-\x7F]/n, '').to_s
     ascii_simple = ascii.gsub(/[^-'a-zA-Z ]/, '').strip
-    ascii_no_articles = ascii_simple.gsub(/^(de +|la +|du +|l' *|d' *)*/i, '')
+    ascii_no_articles = ascii_simple.gsub(/^(de +|des +|la +|du +|l' *|d' *)*/i, '')
     ascii_no_articles.downcase
   end
 end
